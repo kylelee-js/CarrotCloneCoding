60acba2127b33060e3febf5f8191f938306f6898
lee gihun <leegihun@leeui-MacBookAir-2.local>
2021-10-03 20:45:03 +0900

    update added, but it needs refactoring with localstorage JSON object

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAGTUYU27s_Z_I7kdXxZjF9j6syYjNSmXs",
   authDomain: "carrot-3864c.firebaseapp.com",
@@ -16,24 +12,45 @@ const firebaseConfig = {
   messagingSenderId: "312338184212",
   appId: "1:312338184212:web:a26d3f8b1f69c4946f0f01"
 };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
+let container = document.querySelector('.container');
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore();
-
-try {
-    const docRef = await addDoc(collection(db, "Beverage"), {
-      name: "Soda",
-      price: 500,
-      uploadDate: new Date()
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
+const deleteItem = (e) => {
+db.collection("Product").doc(e.target.id).delete().then(() => {
+    console.log("Document successfully deleted!");
+    window.location.reload();
+}).catch((error) => {
+    console.error("Error removing document: ", error);
+});
 }
-  
 
+const updateItem = (e) => {
+window.location.href = "update.html";
+}
+
+db.collection("Product").get().then(a => {
+  a.forEach(doc => {
+    let docu = doc.data();
+    let child = `
+    <p>
+      <div>
+        <div class="name">Product name : ${docu.name}</div>
+        <div class="price">Product price : ${docu.price}$</div>
+        <div class="date">Upload Date : ${docu.uploadDate.toDate().toDateString()}</div>
+      </div>
+      <input type="button" value="Delete Item" class="deleteBtn" id=${doc.id}>
+      <input type="button" value="Edit Item" class="updateBtn">
+    </p>
+    
+    `;
+    container.innerHTML += child;
+  })
+  const deleteBtn = document.querySelector(".deleteBtn");
+  deleteBtn.addEventListener("click", deleteItem);
+}
+);
 
 
 
